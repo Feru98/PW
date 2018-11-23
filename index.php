615af866bd024713327076d9f4d47a35319be331
98teg <teg98tom@gmail.com>
2018-11-23 16:44:36 +0100

    Cambiando el index para que se muestre según la sesión iniciada

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
+<?php
+	session_start();
+echo <<<_END
 <!DOCTYPE html>
 <head>
 	<link rel="stylesheet" type="text/css" href="centroAdopcion.css">
 	<title> Centro de adopcion </title>
 </head>
 <body>
-<?php
+	<table align="right">
+	<tr>
+		<td><a href="./iniciarSesion.php">Iniciar sesion</a></td>
+	</tr>
+	</table>
+_END;
 	require_once('queryFunctionsLibrary.php');
 
 	$q = new ConsultasEmpresa();
@@ -13,51 +21,99 @@
 		echo "Error!";
 		die();
 	}
-echo <<<_END
-<center>
-	<img src="./Dog_Category_Header_Image.png" width="45%"/>
-</center>
-<br>
-<br>
-<br>
-<br>
+	
+	if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+		$now = time();
 
-<table align="center">
-<tr>
-  <th>Nuestros cuidadores:</th>
-  <th></th>
-  <th></th>
-  <th></th>
-  <th></th>
-</tr>
-<tr>
-  <th>DNI</th>
-  <th>Nombre</th>
-  <th></th>
-  <th></th>
-  <th></th>
-</tr>
-_END;
-	$empleados = $q->getEmpleados();
-	foreach($empleados as $empleado){
+		if($now > $_SESSION['expire']){
+			session_destroy();
+			echo "Time out";
+			exit;
+		}
 echo <<<_END
-<tr>
-  <td>$empleado[DNI]</td>
-  <td>$empleado[Nombre]</td>
-  <td><a href="./empleado.php?dni=$empleado[DNI]"><img src="cursor(1).png" width="10%"/></a></td>
-  <td><a href="./editarUsuarioFormulario.php?dni=$empleado[DNI]"><img src="pencil-edit-button.png" width="10%"/></a></td>
-  <td><a href="./eliminarUsuario.php?dni=$empleado[DNI]"><img src="rubbish-bin.png" width="10%"/></a></td>
-</tr>
+		<center>
+			<img src="./Dog_Category_Header_Image.png" width="45%"/>
+		</center>
+		<br>
+		<br>
+		<br>
+		<br>
+
+		<table align="center">
+		<tr>
+		  <th>Nuestros cuidadores:</th>
+		  <th></th>
+		  <th></th>
+		  <th></th>
+		  <th></th>
+		</tr>
+		<tr>
+		  <th>DNI</th>
+		  <th>Nombre</th>
+		  <th></th>
+		  <th></th>
+		  <th></th>
+		</tr>
 _END;
+			$empleados = $q->getEmpleados();
+			foreach($empleados as $empleado){
+echo <<<_END
+		<tr>
+		  <td>$empleado[DNI]</td>
+		  <td>$empleado[Nombre]</td>
+		  <td><a href="./empleado.php?dni=$empleado[DNI]"><img src="cursor(1).png" width="10%"/></a></td>
+		  <td><a href="./editarUsuarioFormulario.php?dni=$empleado[DNI]"><img src="pencil-edit-button.png" width="10%"/></a></td>
+		  <td><a href="./eliminarUsuario.php?dni=$empleado[DNI]"><img src="rubbish-bin.png" width="10%"/></a></td>
+		</tr>
+_END;
+			}
+echo <<<_END
+		<tr>
+		  <td><a href="./agregarUsuarioFormulario.php?dniRepetido="><img src="icon.png" width="10%"/></a></td>
+		   <td><a href="./borrarTodos.php"><img src="delete.png" width="10%"/></a></td>
+		</tr>
+		</table>
+_END;
+
+	}else{
+echo <<<_END
+		<center>
+			<img src="./Dog_Category_Header_Image.png" width="45%"/>
+		</center>
+		<br>
+		<br>
+		<br>
+		<br>
+
+		<table align="center">
+		<tr>
+		  <th>Nuestros cuidadores:</th>
+		  <th></th>
+		</tr>
+		<tr>
+		  <th>DNI</th>
+		  <th>Nombre</th>
+		</tr>
+_END;
+			$empleados = $q->getEmpleados();
+			foreach($empleados as $empleado){
+echo <<<_END
+		<tr>
+		  <td>$empleado[DNI]</td>
+		  <td>$empleado[Nombre]</td>
+		</tr>
+_END;
+			}
+echo <<<_END
+		</table>
+_END;
+		exit;
 	}
-echo <<<_END
-<tr>
-  <td><a href="./agregarUsuarioFormulario.php?dniRepetido="><img src="icon.png" width="10%"/></a></td>
-   <td><a href="./borrarTodos.php"><img src="delete.png" width="10%"/></a></td>
-</tr>
-</table>
-  <a href="./pruebas.php">pruebas</a>
-_END;
+?>
+</body>
+</html>
+
+
 ?>
 </body>
 </html>
--- queryFunctionsLibrary.php
+++ queryFunctionsLibrary.php
@@ -1,5 +1,6 @@
 <?php
 class ConsultasEmpresa {
+/*
 	public $usuario = 'root';
 
 	public $pass = '';
@@ -16,6 +17,28 @@ class ConsultasEmpresa {
 
 		try {
 			$dbc = new PDO('mysql:host=localhost; dbname=empleados', $this->usuario, $this->pass, array(PDO::ATTR_PERSISTENT => true));
+		} catch (PDOException $e) {
+			return null;
+		}
+
+		return $dbc;
+	}
+*/
+	public $usuario = 'i62esgot';
+
+	public $pass = '1234';
+
+	public $dbc;
+
+	public function __construct(){
+		$this->dbc = $this->dbconnect();
+	}
+
+	public function dbconnect(){
+		$dbc = null;
+
+		try {
+			$dbc = new PDO('mysql:host=oraclepr.uco.es; dbname=i62esgot', $this->usuario, $this->pass, array(PDO::ATTR_PERSISTENT => true));
 		} catch (PDOException $e) {
 			return null;
 		}
